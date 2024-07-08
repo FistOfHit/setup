c4d89db4767855c73ac5cfc0fee555f7c360dbd2
FistOfHit <hiteshkumar9@live.co.uk>
2024-07-08 15:05:41 +0100

    Adding setup file

---
+++ setup.sh
@@ -0,0 +1,17 @@
+# Update and upgrade
+sudo apt update && sudo apt upgrade -y 
+
+# Install zsh
+sudo apt install zsh -y
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+# Make risto theme default for ohmyzsh
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="risto"/g' ~/.zshrc
+
+# Install zsh-auto-suggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions)/g' ~/.zshrc
+
+# Install zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sed -i 's/plugins=(git zsh-autosuggestions)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
